b12f13e3bf46b55dfe2c37ae00f418cfc38db01a
Raveen K Sharma <raveen08@gmail.com>
2020-05-17 06:19:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
+
 # Your code goes here!
+
+class Anagram
+  attr_reader :grams
+  @@values = []
+  
+  @@matching = []
+  @@matching2 = []
+  
+  @@final = []
+
+  
+  def initialize(grams)
+   @grams = grams
+   b = grams.clone
+   @@values << b.split
+
+  end 
+
+  def match(grams)
+    
+    grams.each do |result|
+      result.split(" ")
+      if result == @@values.sort.reverse
+        @@final << result
+      else
+        nil 
+      end  
+    end
+    @@final
+
+  end  
+end
